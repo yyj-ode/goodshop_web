17789b30cd82d5ca6222cd802f43df3ee71828b9
lihaowei <you@example.com>
2017-06-07 15:13:44 +0800

    删除一堆乱七八糟的缓存文件

--- storage/framework/views/19e74a5869c1230a7c50f55f952875d3f7ade7b9.php
+++
@@ -1,3 +0,0 @@
-<link href="<?php echo e(asset('assets/frontend/index/web/css/common.css')); ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo e(asset('assets/frontend/index/web/css/index.css')); ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo e(asset('assets/frontend/index/web/css/agreement.css')); ?>" rel="stylesheet" type="text/css" />
--- storage/framework/views/6688a5ade2fd398477808e091e27bc9c2fa62322.php
+++
@@ -1,6 +0,0 @@
-<link href="<?php echo e(asset('assets/frontend/index/web/css/common.css')); ?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo e(asset('assets/frontend/index/web/css/collect.css')); ?>" rel="stylesheet" type="text/css" />
-
-<link rel="stylesheet" href="<?php echo e(asset('assets/frontend/index/web/css/detail.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('assets/frontend/index/web/css/index.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('assets/frontend/index/web/css/live.css')); ?>">
